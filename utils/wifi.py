41232bc5935a7daecd8e4ea44e02d854d07b3e35
Jeff Hegerhorst <jeff.hegerhorst@vivint.com>
2016-11-30 15:50:27 -0700

    Add wifi signal strength retrieval

--- utils/wifi.py
+++ utils/wifi.py
@@ -17,5 +17,58 @@
 # <<
 
 import logging
+import subprocess
 
 logger = logging.getLogger(__name__)
+
+class Wifi(object):
+
+    def __init__(self, device, access_point="blake_drone"):
+        self.device = device
+        self.access_point = access_point
+        self.connected = False
+
+    @property
+    def signal_strength(self):
+        command_parts = ["iw", "dev", self.device, "link", "|", "grep", "signal", "|", "xargs", "|", "awk", "'{print $2}'"]
+        command = " ".join(command_parts)
+
+        #proc = subprocess.Popen(command_parts, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #out, err = proc.communicate()
+
+        #retcode = proc.returncode
+
+        return int(subprocess.check_output(command, shell=True).strip())
+
+    def connect(self, access_point=None):
+        access_point = access_point if access_point else self.access_point
+
+        if access_point is None:
+            raise("Must specify access point")
+
+        self.access_point = access_point
+
+        netctl = True if subprocess.call(["which", "netctl"]) == 0 else False
+        nm = True if subprocess.call(["which", "nmcli"]) == 0 else False
+
+        if netctl:
+            ret = self._netctl_connect(access_point)
+        elif nm:
+            ret = self._nm_connect(access_point)
+        else:
+            raise("Could not find method to connect to access point")
+
+        return ret
+
+    def _netctl_connect(self, access_point):
+        result =  subprocess.call("netctl start {}".format(access_point), shell=True)
+        self.connected = True if result == 0 else False
+        return self.connected
+
+    def _nm_connect(self, access_point):
+        result =  subprocess.call("nmcli c up {}".format(access_point), shell=True)
+        self.connected = True if result == 0 else False
+        return self.connected
+
+    def disconnect(self):
+        raise("Not implemented")
